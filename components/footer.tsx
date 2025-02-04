e3a9a5e19a577121bc7a469ef466f2cc17150b09
Nguezet Jordan Junior <kenfackjordanjunior@gmail.com>
2025-02-04 09:22:35 +0100

    Refactor footer and navbar components for improved structure and accessibility

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
     <footer className="container-base xl:rounded-t-xl bg-slate-900 text-slate-50 py-10 sm:px-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8 md:gap-10">
         <div className="flex flex-col gap-3 w-full">
-          <img src="/logo-dark.png" className="h-20 w-fit" />
+          <img src="logo-dark.png" className="h-20 w-fit" />
           <h4>{"Foundation Jeanne Caroline Mfege"}</h4>
           <p className="font-primary text-sm md:text-base max-w-sm text-slate-300">
             {
@@ -50,9 +50,18 @@ function Footer() {
         </div>
       </div>
       <div className="mt-6 sm:mt-8 pt-6 sm:pt-8 border-t border-slate-800 flex justify-between flex-wrap gap-6 sm:gap-10">
-      <div className="flex flex-col gap-1">
-            <p className="text-slate-300">{`© Copyright 2025 ${config.siteName}. Tous les droits réservés`}</p>
-            <p className="text-slate-400">{"Propulsé par "}<a href="https://krestdev.com" target="_blank" className="font-semibold hover:text-secondary transition-colors duration-300 ease-in-out">{"KrestDev"}</a></p>
+        <div className="flex flex-col gap-1">
+          <p className="text-slate-300">{`© Copyright 2025 ${config.siteName}. Tous les droits réservés`}</p>
+          <p className="text-slate-400">
+            {"Propulsé par "}
+            <a
+              href="https://krestdev.com"
+              target="_blank"
+              className="font-semibold hover:text-secondary transition-colors duration-300 ease-in-out"
+            >
+              {"KrestDev"}
+            </a>
+          </p>
         </div>
         <Link href={"/#don"}>
           <Button size={"main"} variant={"secondary"}>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,43 +1,81 @@
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
-import { ArrowRight, MenuIcon } from 'lucide-react'
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ArrowRight, MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 function Navbar() {
-    const navlinks = [
-        {name: 'Accueil', href: '/'},
-        {name: 'À propos', href: '/a-propos'},
-        {name: 'Services', href: '/#services'},
-        {name: 'Galerie', href: '/galerie'},
-        {name: 'Contact', href: '/contact'}
-    ]
+  const navlinks = [
+    { name: "Accueil", href: "/" },
+    { name: "À propos", href: "/a-propos" },
+    { name: "Services", href: "/#services" },
+    { name: "Galerie", href: "/galerie" },
+    { name: "Contact", href: "/contact" },
+  ];
   return (
-    <div className='w-full h-20 inline-flex items-center justify-center sticky top-0 bg-white z-20'>
-        <div className='inline-flex w-full max-w-(--breakpoint-xl) px-7 items-center justify-between border-b border-gray-300 h-full'>
-            <Link href={"/"}><img src='/logo.png' className='h-12 w-auto' /></Link>
-            <span className='hidden lg:flex items-center gap-3'>
-                {navlinks.map((link, i)=>
-                <Link href={link.href} key={i}><Button variant={"navlink"} className='text-lg'>{link.name}</Button></Link>)}
-            <Link href={"/#don"}><Button size={"main"}>{"Faire un don"}<ArrowRight size={20}/></Button></Link>
-            </span>
-            <Sheet>
-                <SheetTrigger asChild className='inline-flex lg:hidden'>
-                    <Button size={"icon"} variant={"ghost"}><MenuIcon size={24}/></Button>
-                </SheetTrigger>
-                <SheetContent side={"left"}>
-                    <SheetHeader className='h-16 w-full border-b border-gray-200'>
-                        <SheetTitle><img src="/logo.png" className='h-12 w-auto'/></SheetTitle>
-                    </SheetHeader>
-                    <div role="list" className='flex flex-col'>
-                        {navlinks.map((link, i)=><SheetClose asChild key={i}><Link href={link.href}><Button variant={"ghost"} className='h-14 text-lg'>{link.name}</Button></Link></SheetClose>)}
-                        <SheetClose asChild><Link href={"/#don"}><Button size={"main"}>{"Faire un don"}<ArrowRight size={20} strokeWidth={3}/></Button></Link></SheetClose>
-                    </div>
-                </SheetContent>
-            </Sheet>
-        </div>
+    <div className="w-full h-20 inline-flex items-center justify-center sticky top-0 bg-white z-20">
+      <div className="inline-flex w-full max-w-(--breakpoint-xl) px-7 items-center justify-between border-b border-gray-300 h-full">
+        <Link href={"/"}>
+          <img src="/logo.png" alt="nav logo" className="h-12 w-auto" />
+        </Link>
+        <span className="hidden lg:flex items-center gap-3">
+          {navlinks.map((link, i) => (
+            <Link href={link.href} key={i}>
+              <Button variant={"navlink"} className="text-lg">
+                {link.name}
+              </Button>
+            </Link>
+          ))}
+          <Link href={"/#don"}>
+            <Button size={"main"}>
+              {"Faire un don"}
+              <ArrowRight size={20} />
+            </Button>
+          </Link>
+        </span>
+        <Sheet>
+          <SheetTrigger asChild className="inline-flex lg:hidden">
+            <Button size={"icon"} variant={"ghost"}>
+              <MenuIcon size={24} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader className="h-16 w-full border-b border-gray-200">
+              <SheetTitle>
+                <img src="logo.png" alt="logo" className="h-12 w-auto" />
+              </SheetTitle>
+            </SheetHeader>
+            <div role="list" className="flex flex-col">
+              {navlinks.map((link, i) => (
+                <SheetClose asChild key={i}>
+                  <Link href={link.href}>
+                    <Button variant={"ghost"} className="h-14 text-lg">
+                      {link.name}
+                    </Button>
+                  </Link>
+                </SheetClose>
+              ))}
+              <SheetClose asChild>
+                <Link href={"/#don"}>
+                  <Button size={"main"}>
+                    {"Faire un don"}
+                    <ArrowRight size={20} strokeWidth={3} />
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
